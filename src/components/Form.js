b73406b5e731c9e1ee40bc7ce0df0683c57b6a05
Jangwook Ryu <rxjw95@gmail.com>
2021-08-16 23:12:18 +0900

    fix: enter 키 입력시 리스트 등록

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,22 @@
-import React from "react";
-import "./Form.css";
+import React from 'react';
+import './Form.css';
 
-const Form = ({ value, color, onChange, onCreate, onkeyPress }) => {
-  return (
-    <div className="form">
-      <input
-        value={value}
-        onChange={onChange}
-        onKeyPress={onkeyPress}
-        style={{ color }}
-      />
-      <div className="create-button" onClick={onCreate}>
-        등록
-      </div>
-    </div>
-  );
+const Form = ({ value, color, onChange, onCreate }) => {
+    return (
+        <div className='form'>
+            <input
+                value={value}
+                onChange={onChange}
+                onKeyPress={(e) => {
+                    if (e.key === 'Enter') onCreate();
+                }}
+                style={{ color }}
+            />
+            <div className='create-button' onClick={onCreate}>
+                등록
+            </div>
+        </div>
+    );
 };
 
 export default Form;
